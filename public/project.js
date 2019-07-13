e526f5c8f48ebcd1dc90a7cb5bde420809a29447
Chandler <chandler.dekock@yahoo.com>
2019-07-13 16:37:24 +0200

    javascript changes

--- public/project.js
+++ public/project.js
@@ -1,13 +1,27 @@
 var DarkTheme = document.getElementById("Dark")
-
+var index = 2
 $(DarkTheme).click(function(){
 
-    $("body").css( "background","Black")
-    $("body").css( "colour","White")
-    $("section").css("background","Black")
-  })
+  if(index%2 ==0){
+    $("html").css("background-color","Black")
+    $("html").css("color","White")
+    $(DarkTheme).text("Light Mode")
+    index++
+  }
+  else{
+    $("html").css("background-color","White")
+    $("html").css("color","Black")
+    $(DarkTheme).text("Dark Mode")
+    index++
+  }
+})
 
-  function submit(){
-    var nameValue = document.getElementById("form").value;
-    console.log(nameValue)
+  function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
     }
+    alert("Hello there " + x + ". Could I get more marks. Please and thank you")
+  }
+    

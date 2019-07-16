d9744d48b9944602af32eafdbea54148024b5258
Iggy Phiri <egnatiousphiri@gmail.com>
2019-07-16 15:33:53 +0200

    Created a smart contract

---
+++ contracts/smartcontract.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+
+contract titleDeeds{
+    struct PersonalIdentifier {
+        string name;
+        string last_name;
+        uint ID_NumberHash;
+        address UserAddress;
+
+    }
+
+    modifier OnlyOwner (_UserAddress, uint){
+        require(_UserAddress.address = address);
+    }
+
+    function edit()
+  
+    function setUser(string memory _name, string memory _last_name, uint _ID_NumberHash, uint _DOB, string memory _StreetAddress) public {
+       name = _name;
+       last_name = _last_name;
+       ID_NumberHash = _ID_NumberHash;
+       DOB = _DOB;
+       StreetAddress = _StreetAddress;
+
+   }
+
+    function getUser() public view returns (string memory,string memory, uint, uint, string memory) {
+       return (name, last_name, ID_NumberHash, DOB, StreetAddress);
+   }
+
+}
